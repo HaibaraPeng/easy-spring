dc2ace1cf5d555b1ada8e28ad2919c3155be3595
Roc <dongpeng@aeke.com>
2024-12-06 15:49:18 +0800

    feat:增加Cglib动态代理

--- src/test/java/org/example/spring/aop/DynamicProxyTest.java
+++ src/test/java/org/example/spring/aop/DynamicProxyTest.java
@@ -1,10 +1,13 @@
 package org.example.spring.aop;
 
+import org.aspectj.lang.annotation.Before;
 import org.example.spring.aop.aspectj.AspectJExpressionPointcut;
+import org.example.spring.aop.framework.CglibAopProxy;
 import org.example.spring.aop.framework.JdkDynamicAopProxy;
 import org.example.spring.common.WorldServiceInterceptor;
 import org.example.spring.service.WorldService;
 import org.example.spring.service.WorldServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,19 +16,30 @@ import org.junit.jupiter.api.Test;
  */
 public class DynamicProxyTest {
 
-    @Test
-    public void testJdkDynamicProxy() throws Exception {
+    private AdvisedSupport advisedSupport;
+
+    @BeforeEach
+    public void setup() {
         WorldService worldService = new WorldServiceImpl();
 
-        AdvisedSupport advisedSupport = new AdvisedSupport();
+        advisedSupport = new AdvisedSupport();
         TargetSource targetSource = new TargetSource(worldService);
         WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
         MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.example.spring.service.WorldService.explode(..))").getMethodMatcher();
         advisedSupport.setTargetSource(targetSource);
         advisedSupport.setMethodInterceptor(methodInterceptor);
         advisedSupport.setMethodMatcher(methodMatcher);
+    }
 
+    @Test
+    public void testJdkDynamicProxy() throws Exception {
         WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
+    @Test
+    public void testCglibDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
         proxy.explode();
     }
 }
